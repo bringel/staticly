d4f311319592593eaee9207ec414d72e849d233c
Bradley Ringel <brad.ringel@gmail.com>
2013-11-10 18:55:28 -0500

    Initial Commit

---
+++ Staticly/SLDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  SLDetailViewController.h
+//  Staticly
+//
+//  Created by Bradley Ringel on 11/10/13.
+//  Copyright (c) 2013 Bradley Ringel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ StaticlyTests/StaticlyTests.m
@@ -0,0 +1,34 @@
+//
+//  StaticlyTests.m
+//  StaticlyTests
+//
+//  Created by Bradley Ringel on 11/10/13.
+//  Copyright (c) 2013 Bradley Ringel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StaticlyTests : XCTestCase
+
+@end
+
+@implementation StaticlyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
