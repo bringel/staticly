77607fa5739626a0f46ac3358b4993542841af0c
Bradley Ringel <brad.ringel@gmail.com>
2014-02-02 12:59:01 -0600

    We should totally add some unit test things here now.

---
+++ StaticlyTests/SLLoginViewControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  SLLoginViewControllerTests.m
+//  Staticly
+//
+//  Created by Bradley Ringel on 1/31/14.
+//  Copyright (c) 2014 Bradley Ringel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SLLoginViewControllerTests : XCTestCase
+
+@end
+
+@implementation SLLoginViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample{
+}
+
+@end
